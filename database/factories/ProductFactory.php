27b9a6e34ec9a3b443dd4a99975519236c93a3e0
Wiryanto Wijaya <wiryanto.wijaya91@gmail.com>
2021-08-09 12:04:46 +0700

    add mfs Product

---
+++ database/factories/ProductFactory.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Product;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+class ProductFactory extends Factory
+{
+    /**
+     * The name of the factory's corresponding model.
+     *
+     * @var string
+     */
+    protected $model = Product::class;
+
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     */
+    public function definition()
+    {
+        return [
+            //
+            'product_id' => $this->faker->numerify('PD########'),
+            'slug' => $this->faker->slug(),
+            'name' => $this->faker->sentence(mt_rand(2,3)),
+            'category' => $this->faker->sentence(1),
+            'price' => $this->faker->randomNumber(9,false),
+            'picture_path' => $this->faker->imageUrl(360,360,'product',true,'product'),
+            'description' => $this->faker->sentence(mt_rand(5,10)),
+            'status' => $this->faker->boolean(50)
+        ];
+    }
+}
---
+++ database/migrations/2021_08_09_041315_create_products_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateProductsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('products', function (Blueprint $table) {
+            $table->id();
+            $table->string('product_id')->unique();
+            $table->string('slug')->unique();
+            $table->string('name');
+            $table->string('category');
+            $table->decimal('price',10,0);
+            $table->string('picture_path');
+            $table->text('description');
+            $table->boolean('status');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('products');
+    }
+}
